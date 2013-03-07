8b33a8ce2cff5380fbe7bbd8b0b7b26041098a36
Jeremy Seago <seagoj@gmail.com>
2013-03-07 14:24:38 -0600

    Update dotfiles from script

--- script/bootstrap.rb
+++ script/bootstrap.rb
@@ -15,29 +15,51 @@ class Project
                   :output
 
     def initialize(args)
-        # Set instance defaults
-        @project = 'project'
-        @vagrant = nil
-        @docRoot = '/var/www'
-        @codeRoot = '`pwd`'
-        @delete = ['.buildpath','.project','.metadata','nbproject','.settings']
-        @cookbook = @project[0..8]=='cookbook-'
-        @dotfilesPath = 'dotfiles'
-        @dotfilesRepo = 'git@github.com:seagoj/dotfiles.git'
-        @projectRepo = "git@github.com:seagoj/#{@project}.git"
-        @template = {
-            'dotfiles'=>['LICENSE','.gitattributes','.gitignore','CONTRIBUTING.md'],
-            'dirs'=>['src'],
-            'genfiles'=>['README.md',"#{@project}.sublime-project",'.git/hooks/post-receive'],
-            'README.md'=>"## #{@project}",
-            "#{@project}.sublime-project"=>'{"folders":[{"path":"/'+Dir.pwd.gsub(':','')+'/'+@project+'"}]}',
-            '.git/hooks/post-receive'=>"cd ~/code/#{@project} && git pull github master"
-        }
-        @log = File.new('NewProject.log','a+')
-        @output = '## '+Time.now.ctime+"\n"
-        args.each do |k,v|
-            instance_variable_set("@#{k}", v) unless v.nil?
+        puts args
+
+        @project = args[:project] unless args[:project].nil?
+
+        if @project == nil
+            abort("Project not set")
         end
+
+        # Set instance defaults
+        defaults = {
+            :vagrant=>nil,
+            :docRoot=>'/var/www',
+            :codeRoot=>'`pwd`',
+            :delete=>[
+                '.buildpath',
+                '.project',
+                '.metadata',
+                'nbproject',
+                '.settings'
+            ],
+            :dotfilesPath=>'dotfiles',
+            :dotfilesRepo=>'git@github.com:seagoj/dotfiles.git',
+            :log=>File.new('NewProject.log','a+'),
+            :cookbook=> @project[0..8]=='cookbook-',
+            :projectRepo=> "git@github.com:seagoj/#{@project}.git",
+            :template => {
+                'dotfiles'=>['LICENSE','.gitattributes','.gitignore','CONTRIBUTING.md'],
+                'dirs'=>['src'],
+                'genfiles'=>['README.md',"#{@project}.sublime-project",'.git/hooks/post-receive'],
+                'README.md'=>"## #{@project}",
+                "#{@project}.sublime-project"=>'{"folders":[{"path":"/'+Dir.pwd.gsub(':','')+'/'+@project+'"}]}',
+                '.git/hooks/post-receive'=>"cd ~/code/#{@project} && git pull github master"
+            },
+            :output=>'## '+Time.now.ctime+"\n"
+        }
+
+        defaults.merge(args)
+
+        defaults.each do |k,v|
+           instance_variable_set("@#{k}", v) unless v.nil?
+        end
+    end
+
+    def project=(project)
+        @project = project
     end
 
     def clearOldMetadata
@@ -170,135 +192,3 @@ end
 project = Project.new :project => ARGV[0], :vagrant => ARGV[1]
 project.clearOldMetadata()
 project.pullDotfiles()
-
-
-# # # Config#####
-# # project = ARGV[0]
-# # vagrant = ARGV[1]
-# # cookbook = project[0..8]=='cookbook-'
-# # delete = ['.buildpath','.project','.metadata','nbproject','.settings']
-# # dotfilesPath = 'dotfiles'
-# # dotfilesRepo = 'git@github.com:seagoj/dotfiles.git'
-# # projectRepo = "git@github.com:seagoj/#{project}.git"
-# # template = {
-# # 	'dotfiles'=>['LICENSE','.gitattributes','.gitignore','CONTRIBUTING.md'],
-# # 	'dirs'=>['src'],
-# # 	'genfiles'=>['README.md',"#{project}.sublime-project",'.git/hooks/post-receive'],
-# # 	'README.md'=>"## #{project}",
-# # 	"#{project}.sublime-project"=>'{"folders":[{"path":"/'+Dir.pwd.gsub(':','')+'/'+project+'"}]}',
-# # 	'.git/hooks/post-receive'=>"cd ~/code/#{project} && git pull github master"
-# # }
-# # log = File.new('NewProject.log','a+')
-# # output = '## '+Time.now.ctime+"\n"
-# # #############
-
-
-# # unless(File.exists?(dotfilesPath))
-# # 	puts "Pulling dotfiles from #{dotfilesRepo}"
-# # 	output += `git clone #{dotfilesRepo} #{dotfilesPath}`
-# # else
-# # 	# log.info("Fetching dotfiles updates from "+dotfilesRepo)
-# # 	puts "Fetching dotfiles updates from #{dotfilesRepo}"
-# # 	Dir.chdir(dotfilesPath)
-	
-# # 	output += `git add * 2>&1`; result=$?.success?
-
-# # 	# log.write(output+"\n\n")
-	
-# # 	# output = `git commit -a`
-# # 	output += `git commit -m "Update dotfiles from script" 2>&1`; result=$?.success?
-# # 	output += `git fetch 2>&1`; result=$?.success?
-# # 	# log.write(output+"\n\n")
-# # 	output += `git remote add github #{dotfilesRepo} 2>&1`; result=$?.success?
-# # 	# log.write(output+"\n\n")
-
-# # 	branchOutput = `git branch 2>&1`; result=$?.success?
-# # 	branch= branchOutput[2..branchOutput.length-1]
-# # 	# puts dotBranch
-# # 	output+=`git push -u github #{branch} 2>&1`; result=$?.success?
-# # 	Dir.chdir('..')
-# # end
-
-# # unless(File.exists?(project))
-# # 	Dir.mkdir(project)
-# # end
-# # Dir.chdir Dir.pwd+'/'+project.chomp
-
-
-
-
-# # unless(cookbook)
-# # 	template['dirs'].each do |dir|
-# # 		unless(File.exists?(dir))
-# # 			puts "Creating #{dir} directory"
-# # 			Dir.mkdir(dir)
-# # 			if(dir=='src')
-# # 				Dir.chdir('src')
-# # 				index = File.new("index.php","wb")
-# # 				index.write("<?php\n\tprint 'It\\'s alive!';\n")
-# # 				Dir.chdir('..')
-# # 				puts Dir.pwd
-# # 			end
-# # 		end
-# # 	end
-# # end
-
-# # DEMARQUE
-
-# #def populateGit()
-
-# # if(File.exists?('.gitattributes'))
-# # 	File.delete('.gitattributes')
-# # end
-# # if(File.exists?('.gitignore'))
-# # 	File.delete('.gitignore')
-# # end
-
-# # template['dotfiles'].each do |dotfile|
-# # 	unless(File.exists?(dotfile))
-# # 		puts "Copying #{dotfile}"
-# # 		FileUtils.cp('../'+dotfilesPath+'/git/'+dotfile,'.')
-# # 	end
-# # end
-
-# # end #populateGit()
-
-# def genSublimeText()
-
-# # template['genfiles'].each do |gen|
-# # 	unless(File.exists?(gen))
-# # 		puts "Building #{gen}"
-# # 		file = File.new(gen,'wb')
-# # 		file.write(template[gen])
-# # 	end
-# # end
-
-# # FileUtils.chmod 0755, "#{project}/.git/hooks/post-receive"
-
-# end #genProjectFiles()
-
-# def genVagrant()
-
-# # if(vagrant)
-# # 	unless(File.exists?("Vagrantfile"))
-# # 		puts "Copying #{vagrant} Vagrantfile"
-# # 		FileUtils.cp("../#{dotfilesPath}/vagrantfiles/#{vagrant}/Vagrantfile",".")
-# # 	end
-# # end
-
-# end #genVagrant()
-
-# def githubPush()
-
-# # branchOutput=`git branch 2>&1`; result=$?.success?
-# # branch= branchOutput[2..branchOutput.length-1]
-
-# # output += `git init 2>&1`; result=$?.success?
-# # output += `git add * 2>&1`; result=$?.success?
-# # output += `git commit -a -m "Commit dotfiles" 2>&1`; result=$?.success?
-# # output += `git remote add github #{projectRepo} 2>&1`; result=$?.success?
-# # output += `git push -u github #{branch} 2>&1`; result=$?.success?
-
-# # log.write(output)
-
-# end #githubPush()
