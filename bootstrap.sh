b80b27d3f1d7fa71888e5f79a5d41423523d21ad
Jeremy Seago <seagoj@gmail.com>
2015-08-30 15:30:51 +0000

    updates dotfiles for deployment on raspberry pi

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,14 @@ for i in "${SECRETS[@]%.gpg}"; do
     gpg --batch --yes --quiet --output $i --decrypt $i.gpg
 done
 
+if [[ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]]; then
+    if which git >/dev/null; then
+        git clone --recursive git://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" 
+    else 
+        echo "Git must be installed to pull down prezto.";
+    fi
+fi
+
 stow $STOWOPTS ansible\
     anyconnect\
     atom\
