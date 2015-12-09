9fb03793b1e3bc60af7ad17e8f26e0a0a7cf4df3
Jeremy Seago <seagoj@gmail.com>
2015-12-09 16:14:23 -0600

    installs Phar

--- php/_install.sh
+++ php/_install.sh
@@ -2,7 +2,7 @@
 
 if ! which php > /dev/null; then
     osinstall php
-    echo extension=phar.so >> /etc/php/php.ini
+    echo extension=phar.so | sudo tee -a /etc/php/php.ini
 fi
 
 if ! which composer > /dev/null; then
