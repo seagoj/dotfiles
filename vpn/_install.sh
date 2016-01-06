3716fb164c70de39c256119ccb8af603fd6bd9a8
Jeremy Seago <seagoj@gmail.com>
2016-01-06 10:44:49 -0600

    corrects openconnect installation

--- vpn/_install.sh
+++ vpn/_install.sh
@@ -1,4 +1,4 @@
-if ! which openconnect; then
+if ! which openconnect > /dev/null; then
     osinstall openconnect networkmanager-openconnect &&\
         sudo systemctl restart NetworkManager.service
 fi
