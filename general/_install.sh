dc7856cde6ab17259ef25df56852f7ccf85b0457
Jeremy Seago <seagoj@gmail.com>
2016-01-07 12:53:16 -0600

    do not popd

--- general/_install.sh
+++ general/_install.sh
@@ -5,7 +5,6 @@ Arch | Mac | Debian)
     autoload updateRepo; updateRepo git://github.com/powerline/fonts.git fonts | grep "up to date" > /dev/null
     if [[ ! $? -eq 0 ]]; then
         ./install.sh
-        popd
     fi
     ;;
 *)
