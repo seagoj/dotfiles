cf4576a00b63b39f9808c9cbae6364ff498d4ac3
Jeremy Seago <seagoj@gmail.com>
2016-01-11 13:01:25 -0600

    correctly installs submodules

--- ssh/_install.sh
+++ ssh/_install.sh
@@ -1,4 +1,4 @@
 $HOME/bin/resetSshPermissions > /dev/null
 git remote rm origin &&\
     git remote add origin git@github.com:seagoj/dotfiles.git &&\
-    git submodule update
+    git submodule update --recursive --init
