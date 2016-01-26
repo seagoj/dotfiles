50419abecabf530201061bd92cc7368801680713
Jeremy Seago <seagoj@gmail.com>
2016-01-26 15:28:10 -0600

    don't reinstall steam

--- steam/_install.sh
+++ steam/_install.sh
@@ -1,5 +1,5 @@
-# if ! which steam > /dev/null; then
-    # echo "[multilib]" | sudo tee -a /etc/pacman.conf
-    # echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+if ! which steam > /dev/null; then
+    echo "[multilib]" | sudo tee -a /etc/pacman.conf
+    echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
     osinstall multilib-devel lib32-alsa-plugins xf86-video-intel lib32-mesa-libgl steam
-# fi
+fi
