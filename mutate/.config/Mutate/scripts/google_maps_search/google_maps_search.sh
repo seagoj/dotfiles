b93f002b1b21230e88312ebb3924bf91fc6996d9
Jeremy Seago <seagoj@gmail.com>
2016-01-27 15:44:20 -0600

    removes mutate

--- mutate/.config/Mutate/scripts/google_maps_search/google_maps_search.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo [$@]
-echo "command=xdg-open \"https://www.google.com/maps/place/$@\"/"
-echo "icon="
-echo "subtext=Search on google maps for $@"
--- mutate/_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if ! which mutate > /dev/null; then
-    osinstall mutate-git
-fi
