e969c293c9e712071cbae067225528e378f22f24
Jeremy Seago <seagoj@gmail.com>
2016-01-28 18:49:51 -0600

    updates ansible files

--- ansible/_install.sh
+++ ansible/_install.sh
@@ -3,3 +3,11 @@
 if ! which ansible > /dev/null; then
     osinstall ansible
 fi
+
+if [[ ! -f "${HOME}/.vault_pass" ]]; then
+    echo "$(pass ansible/vault/bellevue | head -n1)" > "${HOME}/.vault_pass"
+fi
+
+if [[ ! -f "${HOME}/.vault_pass_doomsday" ]]; then
+echo "$(pass ansible/vault/doomsday | head -n1)" > "${HOME}/.vault_pass_doomsday"
+fi
