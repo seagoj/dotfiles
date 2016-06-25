27680eb3c34703c8574e7d984ec65a3d5376f225
Jeremy Seago <seagoj@gmail.com>
2016-06-25 17:59:47 -0500

    buku

--- buku/_install.sh
+++ buku/_install.sh
@@ -2,12 +2,12 @@ if ! which pip3 >/dev/null; then
     dotfiles::bootstrap python
 fi
 
-sudo pip3 install --upgrade pip >/dev/null
+general::sudo '-H pip3 install --upgrade pip' >/dev/null
 
 if ! pip3 freeze | grep pycrypto >/dev/null; then
-    general::sudo 'pip3 install pycrypto' >/dev/null
+    general::sudo '-H pip3 install pycrypto' >/dev/null
 fi
 
 dotfiles::update_repo https://github.com/jarun/Buku buku >/dev/null
-general::sudo 'make install'
+general::sudo 'make install' >/dev/null
 popd >/dev/null
