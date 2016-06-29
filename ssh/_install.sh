bc673dfaa85b462c08a7150eb4d15d1280213ee4
Jeremy Seago <seagoj@gmail.com>
2016-06-29 11:08:49 -0500

    installs ssh-copy-id

--- ssh/_install.sh
+++ ssh/_install.sh
@@ -1,5 +1,13 @@
 . $HOME/functions/ssh.sh
 
+if ! which ssh >/dev/null; then
+    osinstall ssh
+fi
+
+if ! which ssh-copy-id >/dev/null; then
+    osinstall ssh-copy-id
+fi
+
 ssh::reset_permissions
 
 git remote rm origin &&\
