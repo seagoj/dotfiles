7312601a8cd5bb0de2ae507347090d53b03c92d2
Jeremy Seago <seagoj@gmail.com>
2016-08-24 21:58:38 -0500

    atom config

---
+++ atom/_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+installPackages() {
+	apm install \
+		linter \
+		api-workbench >/dev/null
+}
+
+installMac() {
+	wget https://atom.io/download/mac --output-file=atom-mac.zip &&\
+		unzip atom-mac.zip &&\
+		mv Atom /Applications/ &&\
+		rm atom-mac.zip
+
+	installPackages
+}
+
+dotfiles::install atom
+installPackages
