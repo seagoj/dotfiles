c9d9f478a1e3d29725a55e7cc1b1e32a310afc2a
Jeremy Seago <seagoj@gmail.com>
2016-09-13 12:12:09 -0500

    corrects hyperterm package updating

--- hyperterm/.hyperterm.js
+++ hyperterm/.hyperterm.js
@@ -2,61 +2,61 @@ let semibold = 600;
 let blue = '#6688aa';
 let systemDefaultShell = '';
 let fonts = [
-  "Source Code Pro for Powerline",
-  "Sauce Code Powerline",
-  "Anonymous Pro",
-  "Anonymice Powerline",
-  "Menlo",
-  "DejaVu Sans Mono",
-  "Lucida Console",
-  "monospace"
+	"Source Code Pro for Powerline",
+	"Sauce Code Powerline",
+	"Anonymous Pro",
+	"Anonymice Powerline",
+	"Menlo",
+	"DejaVu Sans Mono",
+	"Lucida Console",
+	"monospace"
 ];
 
 module.exports = {
-  config: {
-    fontSize: 10,
+	config: {
+		fontSize: 10,
 
-    fontFamily: fonts.join(','),
+		fontFamily: fonts.join(','),
 
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
+		// `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+		cursorShape: 'BLOCK',
 
-    // custom css to embed in the main window
-    css: `
-        .hyperterm_main { border: none; }
-    `,
+		// custom css to embed in the main window
+		css: `
+				.hyperterm_main { border: none; }
+		`,
 
-    // custom css to embed in the terminal window
-    termCSS: `
-      x-screen * { font-weight: ` + semibold + `; }
-      x-screen a {
-        color: ` + blue + `;
-        text-decoration: underline;
-      }
-      x-screen a.hover { text-decoration: none; }
-    `,
+		// custom css to embed in the terminal window
+		termCSS: `
+			x-screen * { font-weight: ` + semibold + `; }
+			x-screen a {
+				color: ` + blue + `;
+				text-decoration: underline;
+			}
+			x-screen a.hover { text-decoration: none; }
+		`,
 
-    padding: '0px',
+		padding: '0px',
 
-    shell: systemDefaultShell,
+		shell: systemDefaultShell,
 
-    hyperclean: {
-      hideTabs: true
-    },
+		hyperclean: {
+			hideTabs: true
+		},
 
-    hypertype: false,
+		hypertype: false,
 
-    // for advanced config flags please refer to https://hyperterm.org/#cfg
-  },
+		// for advanced config flags please refer to https://hyperterm.org/#cfg
+	},
 
-  plugins: [
-    'hyperclean',
-    'hyperlinks',
-    'hyperterm-sourcerer',
-    'hyperterm-themed-scrollbar'
-  ],
+	plugins: [
+		'hyperclean',
+		'hyperlinks',
+		'hyperterm-sourcerer',
+		'hyperterm-themed-scrollbar'
+	],
 
-  localPlugins: [
-    'hypertype'
-  ]
+	localPlugins: [
+		'hypertype'
+	]
 };
