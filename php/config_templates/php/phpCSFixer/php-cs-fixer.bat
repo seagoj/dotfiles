2c7c4a407b7f2526441448aefb8fc85f5db7ee33
Jeremy Seago <seagoj@gmail.com>
2016-12-04 23:52:05 -0600

    remove config_templates

--- php/config_templates/php/phpCSFixer/php-cs-fixer.bat
+++
@@ -1,6 +0,0 @@
-SET PHPPATH=C:\wamp\bin\php\php5.3.4
-SET WORKPath=C:\Users\JDS.SFC\Google Drive\workspace
-
-"C:\wamp\bin\php\php5.3.4\php.exe" "C:\Users\JDS.SFC\Google Drive\workspace\Environment\php-cs-fixer.phar" fix "%WORKPATH%\%1\src"
-"%PHPPATH%\php.exe" "%WORKPATH%\Environment\php-cs-fixer.phar" fix "%WORKPATH%\%1\lib"
-"%PHPPATH%\php.exe" "%WORKPATH%\Environment\php-cs-fixer.phar" fix "%WORKPATH%\%1\tests"
