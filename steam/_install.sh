94e9aee1f182d627ba9f6c95e6ff569007fcd21e
Jeremy Seago <seagoj@gmail.com>
2018-04-10 09:26:08 -0500

    wip

--- steam/_install.sh
+++
@@ -1,5 +0,0 @@
-if ! which steam &>/dev/null; then
-    general::sudo $(tee -a /etc/pacman.conf <<< '[multilib]')
-    general::sudo $(tee -a /etc/pacman.conf <<< 'Include = /etc/pacman.d/mirrorlist')
-    osinstall multilib-devel lib32-alsa-plugins xf86-video-intel lib32-mesa-libgl steam
-fi
--- vpn/_install.sh
+++
@@ -1,4 +0,0 @@
-if ! which openconnect &>/dev/null; then
-    osinstall openconnect networkmanager-openconnect &&\
-        general::sudo systemctl restart NetworkManager.service
-fi
