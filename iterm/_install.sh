10d6b6524ffdb96c3ebfcfc8b60357234a6d2210
Jeremy Seago <seagoj@gmail.com>
2018-06-13 22:00:11 -0500

    removes iterm config

--- iterm/_install.sh
+++
@@ -1,4 +0,0 @@
-if [[ ! -f iterm/.iterm/sourcerer.itermcolors ]]; then
-    dotfiles::update_repo https://github.com/xero/sourcerer sourcerer && popd
-    ln -s $CODE/sourcerer/sourcerer.itermcolors iterm/.iterm/sourcerer.itermcolors
-fi
