9f131970de733d83c068f6fea35170598bf4d45c
Jeremy Seago <seagoj@gmail.com>
2019-07-31 20:48:21 -0500

    master: adds surf

--- suckless/.local/functions/suckless.sh
+++ suckless/.local/functions/suckless.sh
@@ -1,9 +1,37 @@
 suckless::applyPatches() {
-    echo $version
+    patchesRoot=https://"${package}".suckless.org/patches
     patches="${1}"
     for p in "${patches[@]}"; do
-        wget "${p}" -O "patch.diff" && \
-            patch -Np1 -i "patch.diff"
+        pNoDashes=$(echo "${p}" | sed 's/-//')
+        diffFile="${package}-${version}-${pNoDashes}.diff"
+        wget "$patchesRoot/${p}/$diffFile" -O "$diffFile" && \
+            patch -Np1 -i "$diffFile"
+    done
+
+    nvim -d config.h config.def.h
+}
+
+suckless::applyPatchesInvert() {
+    patchesRoot=https://"${package}".suckless.org/patches
+    patches="${1}"
+    for p in "${patches[@]}"; do
+        pNoDashes=$(echo "${p}" | sed 's/-//')
+        diffFile="${package}-${pNoDashes}-${version}.diff"
+        wget "$patchesRoot/${p}/$diffFile" -O "$diffFile" && \
+            patch -Np1 -i "$diffFile"
+    done
+
+    nvim -d config.h config.def.h
+}
+
+suckless::applyToolPatches() {
+    patchesRoot="https://tools.suckless.org/${package}/patches"
+    patches="${1}"
+    for p in "${patches[@]}"; do
+        pNoDashes=$(echo "${p}" | sed 's/-//')
+        diffFile="${package}-${pNoDashes}-$version.diff"
+        wget "$patchesRoot/${p}/$diffFile" -O "$diffFile" && \
+            patch -Np1 -i "$diffFile"
     done
 
     nvim -d config.h config.def.h
