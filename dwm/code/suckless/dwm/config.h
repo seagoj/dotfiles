ba6f2dbab5171dc53cae56689d759158d718e198
Jeremy Seago <seagoj@gmail.com>
2020-05-17 09:40:09 -0500

    master: removes gaps and sets modkey for linux

--- dwm/code/suckless/dwm/config.h
+++ dwm/code/suckless/dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -67,8 +67,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-/* #define MODKEY Mod4Mask */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+/* #define MODKEY Mod1Mask */
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
