c110b0a51d357f8a15f012eeb6ac17ebb378f6a2
Jeremy Seago <seagoj@gmail.com>
2020-05-22 15:50:17 -0500

    master: Mod+b opens browser

--- dwm/code/suckless/dwm/config.h
+++ dwm/code/suckless/dwm/config.h
@@ -82,12 +82,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-F", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "firefox-developer-edition", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,      togglebar,      {0} },
+    /* { MODKEY,                       XK_b,      togglebar,      {0} }, */
+    { MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
     /* { MODKEY,                       XK_h,      incnmaster,     {.i = +1 } }, */
