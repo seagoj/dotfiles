7b06bd647d7c759cee3bb184c2e4a458218b007b
Jeremy Seago <seagoj@gmail.com>
2021-01-05 05:00:02 -0600

    dwm-warp: adds mouse warp to dwm
    dwm-warp: https://bonfyreapp.atlassian.net/browse/dwm-warp

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -59,6 +59,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },    /* first entry is default */
+    /* { "><>",      NULL },    /1* no layout function means floating behavior *1/ */
     { "[M]",      monocle },
 };
 
@@ -83,6 +84,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox-developer-edition", NULL };
 static const char *notescmd[]  = { "st", "-e", "scratchpad", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
@@ -94,6 +96,8 @@ static Key keys[] = {
     { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
     { MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
     { MODKEY,                       XK_m,      zoom,           {0} },
     { MODKEY,                       XK_n,      spawn,          {.v = notescmd } },
     { MODKEY,                       XK_Tab,    view,           {0} },
