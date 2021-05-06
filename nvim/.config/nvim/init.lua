b4cb6cdbd028297581c237557384e81a5b4bb548
Jeremy Seago <seagoj@gmail.com>
2021-05-06 15:44:20 -0500

    master: move exrc into init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,3 +17,8 @@ api.nvim_set_keymap('n', t'<leader>r', ':Gsearch', { noremap = true })
 -- php lsp
 require 'lspconfig'.intelephense.setup{ on_attach=require'completion'.on_attach }
 require 'lspconfig'.vuels.setup{}
+-- vim.cmd [[ set exrc ]]
+
+vim.cmd [[ augroup bonfyre ]]
+vim.cmd [[	 au BufNewFile,BufRead */bonfyre_app/** let g:vdebug_options.path_maps = {"/vagrant/bonfyre_app": "~/.local/src/bonfyre/config_management/puppet/bonfyre_app"} ]]
+vim.cmd [[ augroup END ]]
